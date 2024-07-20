3c37e624f25501b1d0b54362ce9313723a47ee23
SalmannTariq <139531025+SalmannTariq@users.noreply.github.com>
2024-07-20 15:47:48 +0500

    Add files via upload

---
+++ final_project/router/booksdb.js
@@ -0,0 +1,15 @@
+let books = {
+      1: {"author": "George Orwell", "title": "1984", "reviews": {} },
+      2: {"author": "J.K. Rowling", "title": "Harry Potter and the Philosopher's Stone", "reviews": {} },
+      3: {"author": "F. Scott Fitzgerald", "title": "The Great Gatsby", "reviews": {} },
+      4: {"author": "J.R.R. Tolkien", "title": "The Hobbit", "reviews": {} },
+      5: {"author": "Harper Lee", "title": "To Kill a Mockingbird", "reviews": {} },
+      6: {"author": "Homer", "title": "The Iliad", "reviews": {} },
+      7: {"author": "Leo Tolstoy", "title": "War and Peace", "reviews": {} },
+      8: {"author": "Gabriel García Márquez", "title": "One Hundred Years of Solitude", "reviews": {} },
+      9: {"author": "Mark Twain", "title": "The Adventures of Huckleberry Finn", "reviews": {} },
+      10: {"author": "Mary Shelley", "title": "Frankenstein", "reviews": {} }
+  };
+
+
+module.exports=books;
